55e1b48bebfe52c7f9e0b95fa1fcf3762b7ecacf
David Juarez <djuar05@gmail.com>
2020-05-26 13:34:39 -0400

    create class, constructor and method for class Person

--- basicAngular.js
+++ basicAngular.js
@@ -3,3 +3,26 @@ function sayHello(person) {
 }
 var person1 = "Super Student";
 document.getElementById("id1").innerHTML = sayHello(person1);
+var Person = /** @class */ (function () {
+    function Person(firstN, lastN, age, phone, state, zipcode, occupation) {
+        this.firstN = firstN;
+        this.lastN = lastN;
+        this.age = age;
+        this.phone = phone;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.occupation = occupation;
+    }
+    //getters
+    Person.prototype.getfullName = function () {
+        return "Full Name: " + this.firstN + this.lastN;
+    };
+    Person.prototype.getNameAndPhone = function () {
+        return this.getfullName() +
+            "\nPhone Number: " + this.phone;
+    };
+    Person.prototype.getLocation = function () {
+        return this.getfullName() + " lives in the state of " + this.state + " witin the zipcode of " + this.zipcode;
+    };
+    return Person;
+}());
--- basicAngular.ts
+++ basicAngular.ts
@@ -5,3 +5,42 @@ function sayHello(person: string): string{
 let person1 = "Super Student";
 
 document.getElementById("id1").innerHTML = sayHello(person1);
+
+class Person{
+
+    private firstN: string;
+    private lastN: string;
+    private age: string;
+    private phone: string;
+    private state: string;
+    private zipcode: string;
+    private occupation: string;
+
+    constructor(firstN: string, lastN: string, age: string, phone: string, state: string, zipcode: string, occupation: string){
+        this.firstN = firstN;
+        this.lastN = lastN;
+        this.age = age;
+        this.phone = phone;
+        this.state= state;
+        this.zipcode = zipcode;
+        this.occupation = occupation;
+    }
+
+    //getters
+    getfullName(): string{
+        return "Full Name: " + this.firstN + this.lastN;
+    }
+
+    getNameAndPhone(): string{
+        return this.getfullName() + 
+        "\nPhone Number: " + this.phone; 
+    }
+
+    getLocation(): string{
+        return this.getfullName() + " lives in the state of " + this.state + " witin the zipcode of " + this.zipcode;
+    }
+
+
+
+
+}
